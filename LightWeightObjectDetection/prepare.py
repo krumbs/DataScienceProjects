73303e6c03d16487d19917df23379dd5ea3acf46
krumbs <stephaniecjonkers@outlook.com>
2020-09-09 14:55:06 +0200

    fixs logic error, adds num_samples parameter

--- LightWeightObjectDetection/prepare.py
+++ LightWeightObjectDetection/prepare.py
@@ -5,22 +5,24 @@ from pathlib import Path
 
 
 
-def create_tf_record(mode):
+def create_tf_record(mode, num_samples):
     cfg = Config()
     if mode == 'train':
-        convert_png_to_tfRecords(mode, cfg.PATH_TO_IMAGES, cfg.PATH_TO_LABEL_MAP, cfg.PATH_TO_LABELS, Path(cfg.PATH_TO_TFRECORDS, 'kitti_train.record'))
+        convert_png_to_tfRecords(mode, num_samples, cfg.PATH_TO_IMAGES, cfg.PATH_TO_LABEL_MAP, cfg.PATH_TO_LABELS, Path(cfg.PATH_TO_TFRECORDS, 'kitti_train.record'))
 
     if mode == 'test':
-        convert_png_to_tfRecords(mode, cfg.PATH_TO_IMAGES_TEST, cfg.PATH_TO_LABEL_MAP, None, Path(cfg.PATH_TO_TFRECORDS, 'kitti_test.record'))
+        convert_png_to_tfRecords(mode, num_samples, cfg.PATH_TO_IMAGES_TEST, cfg.PATH_TO_LABEL_MAP, None, Path(cfg.PATH_TO_TFRECORDS, 'kitti_test.record'))
     print("Prepare data: Done.")
 
-def prepare_data(mode='train'):
+def prepare_data(args):
+    mode = args.mode 
+    num_samples = args.num_samples
     if not mode in ['train', 'test']:
         print("Please choose a valid mode: train or test. Exiting.")
         
     else:
         print(f"Prepare data: creating kitti_{mode}.record.")
-        create_tf_record(mode)
+        create_tf_record(mode, num_samples)
 
 
 if __name__ == '__main__':
@@ -28,8 +30,8 @@ if __name__ == '__main__':
     parser = ArgumentParser(description=('Prepare KITTI dataset in TFRecord format.'))
 
     parser.add_argument('--mode', type=str, default='train', help='create train or test tf record file')
-
+    parser.add_argument('--num_samples', type=int, default=0, help='number of images to record')
     args = parser.parse_args()
 
-    prepare_data(args.mode)
+    prepare_data(args)
 
